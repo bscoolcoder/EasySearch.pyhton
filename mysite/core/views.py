0693e9ea4f431e88aba300637b4549efac111cbc
bscoolcoder <bhawana053@gmail.com>
2018-01-28 11:34:28 +0530

    views.py

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -3,7 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from googleplaces import GooglePlaces, types, lang
+from django.http import HttpResponseRedirect
 
+API_KEY= 'AIzaSyDmrEIL2QYmb0SIpupFSRDV7ruITLWTMTc'
+google_places = GooglePlaces(API_KEY)
 
 @login_required
 def home(request):
@@ -23,4 +27,49 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
-# Create your views here.
+ 
+def search(request):
+    return render_to_response('search.html', context_instance=RequestContext(request))
+		
+def about(request):
+    return render_to_response('about.html', context_instance=RequestContext(request))
+
+def contact(request):
+    errors = []
+    if request.method == 'POST':
+       if not request.POST.get('subject',''):
+              errors.append('Enter a subject.')
+       if not request.POST.get('message',''):
+              errors.append('Enter a message.')
+       if not errors:
+              send_mail(
+            request.POST['subject'],
+  request.POST['message'],
+  request.POST.get('email', 'abc@gmail.com'),
+  ['abc@gmail.com'],
+   )
+       return HttpResponseRedirect('/thanks.html')
+    return render(request, 'contact.html', {'errors': errors})
+ 
+def places(request):
+    #define dictionary
+    mylist = {}
+    #define variables
+    loc = ""
+    key = ""
+    if request.method == 'POST':
+		    loc = request.POST.get('location','')
+		    key = request.POST.get('keyword','')
+    	
+    query_result = google_places.nearby_search(
+       	   location=loc, keyword=key, radius=10000
+	   )
+			
+    for place in query_result.places:
+	     
+	     place.get_details()
+	     mylist[place.name] = [place.formatted_address, place.local_phone_number, place.international_phone_number]
+	
+    return render_to_response('display_list.html', {'location':loc, 'keyword':key, 'mylist':mylist}, context_instance=RequestContext(request))
+
+
